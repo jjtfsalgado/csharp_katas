7b8a8381827bbad908de87520de0621c56ad7ad2
salgadoj <jose.salgado@iplicit.com>
2020-03-02 14:12:51 +0000

    Make complementary dna c#

---
+++ CSharp katas/CSharp katas/002_MakeComplement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp_katas
+{
+    class MakeComplement
+    {
+        public class DNA
+        {
+            public DNA()
+            {
+            }
+
+            public string A { get; set; }
+            public string T { get; set; }
+            public string G { get; set; }
+            public string C { get; set; }
+        }
+
+        public static Dictionary<char, string> mappedValue = new Dictionary<char, string>() {
+            { 'A', "T" },
+            { 'T', "A" },
+            { 'G', "C" },
+            { 'C', "G" }
+        };
+
+        public static string Get(string dna)
+        {
+            var array = dna.ToCharArray();
+
+            var res = array.Select(i => mappedValue[i]);
+
+            return string.Join("", res);
+        }
+
+
+    }
+}
--- CSharp katas/CSharp katas/Program.cs
+++ CSharp katas/CSharp katas/Program.cs
@@ -7,9 +7,13 @@ namespace CSharp_katas
         static void Main(string[] args)
         {
 
-            var result = CategorizeNewMember.OpenOrSenior(new int[][] { new int[] { 18, 20 }, new int[] { 45, 2 }, new int[] { 61, 12 }, new int[] { 37, 6 }, new int[] { 21, 21 }, new int[] { 78, 9 } });
+            //001
+            //var result = CategorizeNewMember.OpenOrSenior(new int[][] { new int[] { 18, 20 }, new int[] { 45, 2 }, new int[] { 61, 12 }, new int[] { 37, 6 }, new int[] { 21, 21 }, new int[] { 78, 9 } });
 
-            Console.WriteLine(string.Join("\t",result));
+
+            //002
+            var result = MakeComplement.Get("ATTGC");
+            Console.WriteLine(result);
 
             Console.WriteLine("Hello World!");
         }
