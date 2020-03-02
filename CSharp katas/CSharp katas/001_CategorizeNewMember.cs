5ae4eebfcf003acc1f30744de650a0cc4cdd8cc4
salgadoj <jose.salgado@iplicit.com>
2020-03-02 13:42:32 +0000

    First kata

---
+++ CSharp katas/CSharp katas/001_CategorizeNewMember.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp_katas
+{
+    class CategorizeNewMember
+    {
+        public static IEnumerable<string> OpenOrSenior(int[][] data)
+        {
+            //your code here
+            return data.Select(i =>
+            {
+                if (i[0] >= 55 && i[1] > 7)
+                {
+                    return "Senior";
+                }
+                else
+                {
+                    return "Open";
+                }
+            }).ToList();
+        }
+    }
+}
---
+++ CSharp katas/CSharp katas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharp_katas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var result = CategorizeNewMember.OpenOrSenior(new int[][] { new int[] { 18, 20 }, new int[] { 45, 2 }, new int[] { 61, 12 }, new int[] { 37, 6 }, new int[] { 21, 21 }, new int[] { 78, 9 } });
+
+            Console.WriteLine(string.Join("\t",result));
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
